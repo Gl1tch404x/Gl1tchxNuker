67478ede712847d9c7774662cd7187bdb83b3f17
Gl1tchx <Gl1tchx4042@outlook.com>
2025-12-12 13:43:35 +0000

    Update logo.py

--- logo.py
+++ logo.py
@@ -7,21 +7,15 @@ PURPLE = Fore.MAGENTA
 
 def display_logo():
     logo = f"""
-{PURPLE}██████╗ {PURPLE} ██╗ {PURPLE}     ██╗{PURPLE}████████╗ {PURPLE}██████╗{PURPLE}██╗  ██╗{PURPLE}███████╗{PURPLE}██████╗ {PURPLE}
-██╔════╝ {PURPLE}██║ {PURPLE}     ██║{PURPLE}╚══██╔══╝{PURPLE}██╔════╝{PURPLE}██║  ██║{PURPLE}██╔════╝{PURPLE}██╔══██╗{PURPLE}
-██║  ███╗{PURPLE}██║ {PURPLE}     ██║{PURPLE}   ██║   {PURPLE}██║     {PURPLE}███████║{PURPLE}█████╗  {PURPLE}██║  ██║{PURPLE}
-██║   ██║{PURPLE}██║ {PURPLE}     ██║{PURPLE}   ██║   {PURPLE}██║     {PURPLE}██╔══██║{PURPLE}██╔══╝  {PURPLE}██║  ██║{PURPLE}
-╚██████╔╝{PURPLE}███████╗{PURPLE} ██║{PURPLE}   ██║   {PURPLE}╚██████╗{PURPLE}██║  ██║{PURPLE}███████╗{PURPLE}██████╔╝{PURPLE}
- ╚═════╝ {PURPLE}╚══════╝{PURPLE} ╚═╝{PURPLE}   ╚═╝    {PURPLE}╚═════╝{PURPLE}╚═╝  ╚═╝{PURPLE}╚══════╝{PURPLE}╚═════╝ {PURPLE}
-                                                            
-████████╗ {PURPLE}██████╗  {PURPLE}██████╗ {PURPLE}██╗     {PURPLE}███████╗{PURPLE}                  
-╚══██╔══╝{PURPLE}██╔═══██╗{PURPLE}██╔═══██╗{PURPLE}██║     {PURPLE}██╔════╝{PURPLE}                  
-   ██║   {PURPLE}██║   ██║{PURPLE}██║   ██║{PURPLE}██║     {PURPLE}███████╗{PURPLE}                  
-   ██║   {PURPLE}██║   ██║{PURPLE}██║   ██║{PURPLE}██║     {PURPLE}╚════██║{PURPLE}                  
-   ██║   {PURPLE}╚██████╔╝{PURPLE}╚██████╔╝{PURPLE}███████╗{PURPLE}███████║{PURPLE}                  
-   ╚═╝    {PURPLE}╚═════╝  {PURPLE}╚═════╝ {PURPLE}╚══════╝{PURPLE}╚══════╝{PURPLE}                  
+        {PURPLE}
+  ________.__  ____  __         .__           ___________           .__          
+ /  _____/|  |/_   |/  |_  ____ |  |__ ___  __\__    ___/___   ____ |  |   ______
+/   \  ___|  | |   \   __\/ ___\|  |  \\  \/  / |    | /  _ \ /  _ \|  |  /  ___/
+\    \_\  \  |_|   ||  | \  \___|   Y  \>    <  |    |(  <_> |  <_> )  |__\___ \ 
+ \______  /____/___||__|  \___  >___|  /__/\_ \ |____| \____/ \____/|____/____  >
+        \/                    \/     \/      \/                               \/          
+         {PURPLE}                  
                             
-
 {PURPLE}Creator: Glitched Tools
  Glitched Tools Discord Server Nuking Tool
 {Style.RESET_ALL}
